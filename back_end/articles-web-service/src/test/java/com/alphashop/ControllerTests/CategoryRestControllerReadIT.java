1c5c1f09a77670178d26042f15ab1cc9a6a75605
cibs84 <d.tilesi@gmail.com>
2024-08-16 13:48:01 +0700

    back_end: test of /categories and /vat-list endpoints

---
+++ back_end/articles-web-service/src/test/java/com/alphashop/ControllerTests/CategoryRestControllerReadIT.java
@@ -0,0 +1,24 @@
+package com.alphashop.ControllerTests;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.alphashop.test.BaseSpringIT;
+
+public class CategoryRestControllerReadIT extends BaseSpringIT {
+
+	@Test
+	public void listAllCategories() throws Exception {
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/categories")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(13)))
+		.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)).andReturn();
+	}
+
+}
