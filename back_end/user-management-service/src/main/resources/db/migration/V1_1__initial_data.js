4f1bf44dcd211abd71d767960b1a3c809bba4046
cibs84 <d.tilesi@gmail.com>
2025-01-08 15:33:59 +0700

    Initial data migration for user-management-service

--- back_end/user-management-service/src/main/resources/db/migration/V1_1__initial_data.js
+++ back_end/user-management-service/src/main/resources/db/migration/V1_1__initial_data.js
@@ -1,4 +1,4 @@
-db.users.drop(); -- Elimina la collezione se esiste
+db.users.drop(); // Elimina la collezione se esiste
 
 db.users.insertMany([
     {
@@ -14,7 +14,7 @@ db.users.insertMany([
         "id": "66f59be7434c31046d1766e2",
         "userId": "evaristo",
         "password": "$2a$10$lH6idMwg4Fesx2wSGdsBju7VzV8OiNhN4EXa6PUBKMgv7N6iZCc9e",
-        "active successive",
+        "active": true,
         "roles": [
             "USER"
         ]
