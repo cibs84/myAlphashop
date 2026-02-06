f7cb9ce77ca21b381760f6964aa8d3d79ff554a3
cibs84 <d.tilesi@gmail.com>
2026-02-06 02:28:01 +0100

    test(articles-web-service): fix integration tests for ArticleRestControllerCreateIT

--- back_end/articles-web-service/src/test/java/com/alphashop/articles_web_service/controllers/ArticleRestControllerCreateIT.java
+++ back_end/articles-web-service/src/test/java/com/alphashop/articles_web_service/controllers/ArticleRestControllerCreateIT.java
@@ -16,50 +16,73 @@ import com.alphashop.articles_web_service.repositories.ArticleRepository;
 import com.alphashop.articles_web_service.test.BaseSpringIT;
 
 class ArticleRestControllerCreateIT extends BaseSpringIT {
-    
+
 	@Autowired
 	private ArticleRepository articleRepository;
-	
+
 	private String JsonData = """
-	{
-	    "codart": "123Test",
-	    "description": "ARTICOLO UNIT TEST INSERIMENTO",
-	    "um": "PZ",
-	    "codStat": "TESTART",
-	    "pcsCart": 6,
-	    "netWeight": 1.75,
-	    "idArtStatus": "1",
-	    "creationDate": "2019-05-14",
-	    "barcodes": [
-	        {
-	            "barcode": "12345678",
-	            "type": "CP"
-	        }
-	    ],
-	    "ingredients": {
-	        "codart": "123Test",
-	        "info": "TEST INGREDIENTI"
-	    },
-	    "category": {
-	        "id": 1,
-	        "description": "DROGHERIA ALIMENTARE"
-	    },
-	    "vat": {
-	        "idVat": 22,
-	        "description": "IVA RIVENDITA 22%",
-	        "taxRate": 22
-	    }
-	}""";
+			{
+			    "codart": "123Test",
+			    "description": "ARTICOLO UNIT TEST INSERIMENTO",
+			    "codStat": "TESTART",
+			    "price": 0,
+			    "um": "PZ",
+			    "pcsCart": 6,
+			    "netWeight": 1.75,
+			    "idArtStatus": 1,
+			    "category": {
+			        "id": 1
+			    },
+			    "vat": {
+			        "idVat": 22
+			    },
+			    "ingredients": {
+			        "codart": "123Test",
+			        "info": "TEST INGREDIENTI"
+			    },
+			    "barcodes": [
+			        {
+			            "barcode": "12345678",
+			            "idTypeArt": "CP"
+			        }
+			    ]
+			}""";
+
+	// TODO Delete it and replace it with JsonData when the price
+	// will be introduced in response (now the price in response is null).
+	private String JsonDataResponse = """
+			{
+			    "codart": "123Test",
+			    "description": "ARTICOLO UNIT TEST INSERIMENTO",
+			    "codStat": "TESTART",
+			    "um": "PZ",
+			    "pcsCart": 6,
+			    "netWeight": 1.75,
+			    "idArtStatus": 1,
+			    "category": {
+			        "id": 1
+			    },
+			    "vat": {
+			        "idVat": 22
+			    },
+			    "ingredients": {
+			        "codart": "123Test",
+			        "info": "TEST INGREDIENTI"
+			    },
+			    "barcodes": [
+			        {
+			            "barcode": "12345678",
+			            "idTypeArt": "CP"
+			        }
+			    ]
+			}""";
 
 	@Test
 	public void testCreateArticle() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles/create")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(JsonData)
-				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
-				.andExpect(content().json(JsonData))
-				.andReturn();
-		
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles").contentType(MediaType.APPLICATION_JSON)
+				.content(JsonData).accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
+				.andExpect(content().json(JsonDataResponse)).andReturn();
+
 		Article article = articleRepository.findByCodart("123Test").get();
 		assertThat(article.getDescription()).isEqualTo("ARTICOLO UNIT TEST INSERIMENTO");
 	}
@@ -67,71 +90,53 @@ class ArticleRestControllerCreateIT extends BaseSpringIT {
 	@Test
 	public void testErrCreateExistingArticle() throws Exception {
 		{
-			mockMvc.perform(MockMvcRequestBuilders.post("/api/articles/create")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(JsonData)
-					.accept(MediaType.APPLICATION_JSON))
-					.andExpect(status().isCreated())
+			mockMvc.perform(MockMvcRequestBuilders.post("/api/articles").contentType(MediaType.APPLICATION_JSON)
+					.content(JsonData).accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
 					.andDo(print());
-			
+
 			Article article = articleRepository.findByCodart("123Test").get();
 			assertThat(article.getDescription()).isEqualTo("ARTICOLO UNIT TEST INSERIMENTO");
 		}
-		
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles/create")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(JsonData)
-				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isConflict())
-				.andExpect(jsonPath("$.status").value(409))
-				.andExpect(jsonPath("$.message")
-						.value("Article '123Test - ARTICOLO UNIT TEST INSERIMENTO' already exists"))
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles").contentType(MediaType.APPLICATION_JSON)
+				.content(JsonData).accept(MediaType.APPLICATION_JSON)).andExpect(status().isConflict())
+				.andExpect(jsonPath("$.status").value(409)).andExpect(jsonPath("$.code").value("ITEM_ALREADY_EXISTS"))
 				.andDo(print());
 	}
 
 	String ErrJsonData = """
-	{
-	    "codart": "123Test",
-	    "description": "asd",
-	    "um": "PZ",
-	    "codStat": "TESTART",
-	    "pcsCart": 6,
-	    "netWeight": 1.75,
-	    "idArtStatus": "1",
-	    "creationDate": "2019-05-14",
-	    "barcodes": [
-	        {
-	            "barcode": "12345678",
-	            "type": "CP"
-	        }
-	    ],
-	    "ingredients": {
-	        "codart": "123Test",
-	        "info": "TEST INGREDIENTI"
-	    },
-	    "category": {
-	        "id": 1,
-	        "description": "DROGHERIA ALIMENTARE"
-	    },
-	    "vat": {
-	        "idVat": 22,
-	        "description": "IVA RIVENDITA 22%",
-	        "taxRate": 22
-	    }
-	}""";
+			{
+			    "codart": "123Test",
+			    "description": "asd",
+			    "codStat": "TESTART",
+			    "price": 0,
+			    "um": "PZ",
+			    "pcsCart": 6,
+			    "netWeight": 1.75,
+			    "idArtStatus": 1,
+			    "category": {
+			        "id": 1
+			    },
+			    "vat": {
+			        "idVat": 22
+			    },
+			    "ingredients": {
+			        "codart": "123Test",
+			        "info": "TEST INGREDIENTI"
+			    },
+			    "barcodes": [
+			        {
+			            "barcode": "12345678",
+			            "idTypeArt": "CP"
+			        }
+			    ]
+			}""";
 
 	@Test
 	public void testErrCreateArticleWithInvalidDescription() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles/create")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(ErrJsonData)
-				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isUnprocessableEntity())
-				.andExpect(jsonPath("$.status").value(422))
-				.andExpect(jsonPath("$.message")
-						.value("Validation error"))
-				.andExpect(jsonPath("$.errorValidationMap.description[0]")
-						.value("The description field must have a number of characters between 6 and 80"))
-				.andDo(print());
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles").contentType(MediaType.APPLICATION_JSON)
+				.content(ErrJsonData).accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnprocessableEntity())
+				.andExpect(jsonPath("$.status").value(422)).andExpect(jsonPath("$.code").value("VALIDATION_ERROR"))
+				.andExpect(jsonPath("$.errorValidationMap.description[0]").value("MIN_LENGTH")).andDo(print());
 	}
 }
