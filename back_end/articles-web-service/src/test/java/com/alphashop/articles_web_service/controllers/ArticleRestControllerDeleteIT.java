c0b879d99c13a0b4bbee44f9eb8e2ad79384a268
cibs84 <d.tilesi@gmail.com>
2026-02-06 03:38:07 +0100

    test(articles-web-service): fix integration tests for ArticleRestControllerDeleteIT

--- back_end/articles-web-service/src/test/java/com/alphashop/articles_web_service/controllers/ArticleRestControllerDeleteIT.java
+++ back_end/articles-web-service/src/test/java/com/alphashop/articles_web_service/controllers/ArticleRestControllerDeleteIT.java
@@ -26,86 +26,84 @@ class ArticleRestControllerDeleteIT extends BaseSpringIT {
 	
 	private String JsonData = """
 	{
-	  "codart": "123Test",
-	  "description": "ARTICOLO UNIT TEST INSERIMENTO",
-	  "um": "PZ",
-	  "codStat": "TESTART",
-	  "pcsCart": 6,
-	  "netWeight": 1.75,
-	  "idArtStatus": "1",
-	  "creationDate": "2019-05-14",
-	  "barcodes": [{
-	      "barcode": "12345678",
-	      "type": "CP"
-	    }],
-	  "ingredients": {
 	    "codart": "123Test",
-	    "info": "TEST INGREDIENTI"
-	  },
-	  "category": {
-	    "id": 1,
-        "description": "DROGHERIA ALIMENTARE"
-	  },
-      "vat": {
-        "idVat": 22,
-        "description": "IVA RIVENDITA 22%",
-        "taxRate": 22
-      }
+	    "description": "ARTICOLO UNIT TEST INSERIMENTO",
+	    "price": 0,
+	    "codStat": "TESTART",
+	    "um": "PZ",
+	    "pcsCart": 6,
+	    "netWeight": 1.75,
+	    "idArtStatus": 1,
+	    "category": {
+	        "id": 1
+	    },
+	    "vat": {
+	        "idVat": 22
+	    },
+	    "ingredients": {
+	        "codart": "123Test",
+	        "info": "TEST INGREDIENTI"
+	    },
+	    "barcodes": [
+	        {
+	            "barcode": "12345678",
+	            "idTypeArt": "CP"
+	        }
+	    ]
 	}""";
 	
 	private String JsonData02 = """
 	{
-	  "codart": "abcTest",
-	  "description": "ARTICOLO UNIT TEST INSERIMENTO",
-	  "um": "PZ",
-	  "codStat": "TESTART",
-	  "pcsCart": 6,
-	  "netWeight": 1.75,
-	  "idArtStatus": "1",
-	  "creationDate": "2019-05-14",
-	  "barcodes": [{
-	      "barcode": "12345678",
-	      "type": "CP"
-	    }],
-	  "ingredients": {
 	    "codart": "abcTest",
-	    "info": "TEST INGREDIENTI"
-	  },
-	  "category": {
-	    "id": 1,
-        "description": "DROGHERIA ALIMENTARE"
-	  },
-      "vat": {
-        "idVat": 22,
-        "description": "IVA RIVENDITA 22%",
-        "taxRate": 22
-      }
+	    "description": "ARTICOLO UNIT TEST INSERIMENTO",
+	    "codStat": "TESTART",
+	    "price": 0,
+	    "um": "PZ",
+	    "pcsCart": 6,
+	    "netWeight": 1.75,
+	    "idArtStatus": 1,
+	    "category": {
+	        "id": 1
+	    },
+	    "vat": {
+	        "idVat": 22
+	    },
+	    "ingredients": {
+	        "codart": "abcTest",
+	        "info": "TEST INGREDIENTI"
+	    },
+	    "barcodes": [
+	        {
+	            "barcode": "12345678",
+	            "idTypeArt": "CP"
+	        }
+	    ]
 	}""";
 
 	
 	@Test
 	public void testDelArticle() throws Exception {
+		
+	String codart = "123Test";
+		
 	{
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles/create")
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(JsonData)
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isCreated())
 				.andDo(print());
 		
-		Article article = articleRepository.findByCodart("123Test").get();
+		Article article = articleRepository.findByCodart(codart).get();
 		assertThat(article.getDescription()).isEqualTo("ARTICOLO UNIT TEST INSERIMENTO");
 	}
 	
 	    Long articoloInitCount = articleRepository.count();
 	    
 	    mockMvc.perform(
-				MockMvcRequestBuilders.delete("/api/articles/delete/123Test")
+				MockMvcRequestBuilders.delete("/api/articles/" + codart)
 				.accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$.status").value("200 OK"))
-		.andExpect(jsonPath("$.message")
-				.value("Deleting article '123Test - ARTICOLO UNIT TEST INSERIMENTO' performed successfully"))
+		.andExpect(status().isNoContent())
 		.andDo(print());
 	
 	    Assertions.assertThat(articleRepository.count()).isEqualTo(articoloInitCount - 1);
@@ -113,37 +111,39 @@ class ArticleRestControllerDeleteIT extends BaseSpringIT {
 	
 	@Test
 	public void testDelArticleNotDeletable() throws Exception {
+		
+	String notDeletableCodart = "abcTest";
+		
 	{
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles/create")
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/articles")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(JsonData02)
 				.accept(MediaType.APPLICATION_JSON))
+		.andDo(print())
 				.andExpect(status().isCreated())
 				.andDo(print());
 		
-		Article article = articleRepository.findByCodart("abcTest").get();
+		Article article = articleRepository.findByCodart(notDeletableCodart).get();
 		assertThat(article.getDescription()).isEqualTo("ARTICOLO UNIT TEST INSERIMENTO");
 	}
 		
 		mockMvc.perform(
-				MockMvcRequestBuilders.delete("/api/articles/delete/abcTest")
+				MockMvcRequestBuilders.delete("/api/articles/" + notDeletableCodart)
 				.accept(MediaType.APPLICATION_JSON))
 		.andExpect(status().isForbidden())
 		.andExpect(jsonPath("$.status").value(403))
-		.andExpect(jsonPath("$.message")
-				.value("Article 'abcTest' not deletable"))
+		.andExpect(jsonPath("$.code").value("NOT_DELETABLE"))
 		.andDo(print());
 	}
 	
 	@Test
 	public void testDelArticleNotFound() throws Exception {
 		mockMvc.perform(
-				MockMvcRequestBuilders.delete("/api/articles/delete/xxxx")
+				MockMvcRequestBuilders.delete("/api/articles/inexistent-codart")
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNotFound())
 				.andExpect(jsonPath("$.status").value(404))
-				.andExpect(jsonPath("$.message")
-						.value("Article to be deleted 'xxxx' was not found"))
+				.andExpect(jsonPath("$.code").value("ITEM_NOT_FOUND"))
 				.andDo(print());
 	}
 }
